8c6743f67a42cdec2b464a1d8a40a86b4294a70f
Rutu Thakkar <rutu.thakkar@iviewlabs.net>
2022-02-22 18:20:20 +0530

    Added cloudinary

--- controller/audioController.js
+++ controller/audioController.js
@@ -2,10 +2,16 @@ const db = require("../models");
 const path = require("path");
 const axios = require("axios");
 const { getAudioDurationInSeconds } = require("get-audio-duration");
-const httpAdapter = require("axios/lib/adapters/http");
+const cloudinary = require("cloudinary").v2;
+
+cloudinary.config({
+  cloud_name: "rutu",
+  api_key: "372743348695928",
+  api_secret: "sg82sPOO5F9Fe2gWZKK9V7EDMoI",
+});
 
 //Get Audio
-exports.getAudios = (req, res) => {
+const getAudios = (req, res) => {
   // res.json({ message: "hey" });
   // res.send("heyy");
   db.audioDetails.findAll().then((audios) => {
@@ -16,83 +22,120 @@ exports.getAudios = (req, res) => {
     // res.send(audios);
     // console.log(audios);
     res.render("viewAudio", { audios });
+    // res.json({ audios });
   });
 };
 
-// Add Audio File
-exports.addAudio = (req, res) => {
-  // res.send("hey");
+const addAudio = (req, res) => {
+  cloudinary.uploader.upload(
+    req.file.path,
+    {
+      resource_type: "raw",
+      public_id: `AudioUploads/${Date.now() + req.file.originalname}`,
+    },
 
-  // console.log(req);
-
-  if (
-    req.file.mimetype === "audio/basic" ||
-    req.file.mimetype === "audio/mpeg" ||
-    req.file.mimetype === "audio/vnd.wav" ||
-    req.file.mimetype === "audio/vorbis" ||
-    req.file.mimetype === "audio/ogg"
-  ) {
-    getAudioDurationInSeconds(req.file.path)
-      .then((duration) => {
-        // if (duration < 60) {
-        //   duration = duration;
-        //   duration = duration.toFixed(3) + " seconds";
-        // } else if (duration > 60) {
-        //   duration = duration / 60;
-        //   duration = duration.toFixed(2) + " minutes";
-        // }
+    // Send cloudinary response or catch error
+    (err, audio) => {
+      if (err) return res.send(err);
+      // fs.unlinkSync(req.file.path);
+      // res.send(audio);
+      console.log(audio.url);
+      getAudioDurationInSeconds(audio.url).then((duration) => {
         db.audioDetails
           .create({
-            audioFile: req.file.path,
+            audioFile: audio.url,
             audioLength: duration,
           })
           .then((data) => {
             res.json({ message: "File Uploaded!", data });
+          })
+          .catch((err) => {
+            res.json({ error: err.message });
           });
-      })
-      .catch((err) => {
-        console.error(err);
       });
-  } else {
-    res.json({ message: "File type must be audio" });
-  }
+    }
+  );
 };
+
+// Add Audio File
+// const addAudio = async (req, res) => {
+//   // res.send("hey");
+
+//   console.log(req.file);
+
+// if (
+//   req.file.mimetype === "audio/basic" ||
+//   req.file.mimetype === "audio/mpeg" ||
+//   req.file.mimetype === "audio/vnd.wav" ||
+//   req.file.mimetype === "audio/vorbis" ||
+//   req.file.mimetype === "audio/ogg"
+// ) {
+//   getAudioDurationInSeconds(req.file.path)
+//     .then((duration) => {
+// if (duration < 60) {
+//   duration = duration;
+//   duration = duration.toFixed(3) + " seconds";
+// } else if (duration > 60) {
+//   duration = duration / 60;
+//   duration = duration.toFixed(2) + " minutes";
+// }
+//   db.audioDetails
+//     .create({
+//       audioFile: req.file.path,
+//       audioLength: duration,
+//     })
+//     .then((data) => {
+//       res.json({ message: "File Uploaded!", data });
+//     });
+// })
+// .catch((err) => {
+//   console.error(err);
+// });
+// } else {
+//   res.json({ message: "File type must be audio" });
+// }
+// };
 
 //Play Audio
-exports.getAudio = (req, res) => {
-  const { s } = req.params;
-  // const filePath = path.join("../uploads/", s);
-  const filePath = path.join("../uploads/", s);
-  console.log(filePath);
-  res.render("singleAudio", { fileName: s });
+// exports.getAudio = (req, res) => {
+//   const { s } = req.params;
+//   // const filePath = path.join("../uploads/", s);
+//   const filePath = path.join("../uploads/", s);
+//   console.log(filePath);
+//   // res.json({
+//   //   filePath: filePath,
+//   // });
+//   res.render("singleAudio", { fileName: s });
 
-  // axios
-  //   .get(, {
-  //     responseType: "stream",
-  //     adapter: httpAdapter,
-  //     "Content-Range": "bytes 16561-8065611",
-  //   })
-  //   .then((Response) => {
-  //     const stream = Response.data;
+// axios
+//   .get(, {
+//     responseType: "stream",
+//     adapter: httpAdapter,
+//     "Content-Range": "bytes 16561-8065611",
+//   })
+//   .then((Response) => {
+//     const stream = Response.data;
 
-  //     res.set("content-type", "audio/mp3");
-  //     res.set("accept-ranges", "bytes");
-  //     res.set("content-length", Response.headers["content-length"]);
-  //     console.log(Response);
+//     res.set("content-type", "audio/mp3");
+//     res.set("accept-ranges", "bytes");
+//     res.set("content-length", Response.headers["content-length"]);
+//     console.log(Response);
 
-  //     stream.on("data", (chunk) => {
-  //       res.write(chunk);
-  //     });
+//     stream.on("data", (chunk) => {
+//       res.write(chunk);
+//     });
 
-  //     stream.on("error", (err) => {
-  //       res.sendStatus(404);
-  //     });
+//     stream.on("error", (err) => {
+//       res.sendStatus(404);
+//     });
 
-  //     stream.on("end", () => {
-  //       res.end();
-  //     });
-  //   })
-  //   .catch((Err) => {
-  //     console.log(Err.message);
-  //   });
-};
+//     stream.on("end", () => {
+//       res.end();
+//     });
+//   })
+//   .catch((Err) => {
+//     console.log(Err.message);
+//   });
+// };
+
+module.exports = { addAudio, getAudios };
--- routes/audioRoutes.js
+++ routes/audioRoutes.js
@@ -1,7 +1,20 @@
 const express = require("express");
+const app = express();
 const route = express.Router();
 const audioController = require("../controller/audioController");
 const multer = require("multer");
+const { addAudio, getAudios } = require("../controller/audioController");
+const fileUpload = require("express-fileupload");
+const streamifier = require("streamifier");
+const fs = require("fs");
+
+// app.use(
+//   fileUpload({
+//     useTempFiles: true,
+//   })
+// );
+
+
 
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
@@ -13,10 +26,69 @@ const storage = multer.diskStorage({
 });
 
 var upload = multer({ storage: storage });
+// var upload = multer();
 route.get("/", (req, res) => {
   res.render("addAudio");
 });
-route.get("/getAudios", audioController.getAudios);
-route.post("/addAudio", upload.single("audioFile"), audioController.addAudio);
-route.get("/assets/uploads/:s", audioController.getAudio);
+route.get("/getAudios", getAudios);
+route.post("/addAudio", upload.single("audioFile"), addAudio);
+
+  // cloudinary.uploader.upload((error, response) => {
+  //   if (error) {
+  //     console.log(error);
+  //     return;
+  //   }
+  //   console.log(response);
+  //   res.send("File Uploaded");
+  // });
+
+  //   let streamUpload = (req) => {
+  //     return new Promise((resolve, reject) => {
+  //       let stream = cloudinary.uploader.upload_stream((error, result) => {
+  //         if (result) {
+  //           resolve(result);
+  //         } else {
+  //           reject(error);
+  //         }
+  //       });
+  //       streamifier.createReadStream(req.file.buffer).pipe(stream);
+  //     });
+  //   };
+
+  //   async function upload(req) {
+  //     let result = await streamUpload(req);
+  //     // console.log(result);
+  //     res.send("File Uploaded");
+  //   }
+
+  //   upload(req);
+
+//   return new Promise((resolve, reject) => {
+//     let stream = cloudinary.uploader.upload_stream((error, result) => {
+//       if (result) {
+//         resolve(result);
+//       } else {
+//         reject(error);
+//       }
+//     });
+//     console.log(stream);
+
+//     streamifier.createReadStream(req.file.filename).pipe(stream);
+//   });
+// };
+
+// async function upload(req) {
+//   let result = await streamUpload(req)
+//     .then((result) => {
+//       console.log(result);
+//     })
+//     .catch((error) => {
+//       console.error(error);
+//     });
+// }
+
+// upload(req);
+// res.send("Success");
+// });
+// route.get("/assets/uploads/:s", audioController.getAudio);
 module.exports = route;
