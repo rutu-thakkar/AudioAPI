0948ea0db9bfa1627d40cf42e28aabdfecfbf978
Rutu Thakkar <rutu.thakkar@iviewlabs.net>
2022-02-23 18:02:44 +0530

    cors change

--- server.js
+++ server.js
@@ -8,27 +8,27 @@ const db = require("./models");
 const path = require("path");
 const bodyParser = require("body-parser");
 
-var whitelist = [
-  "https://audio--manager.herokuapp.com",
-  "http://localhost:3000/audio/getAudios",
-];
-var corsOptions = {
-  origin: function (origin, callback) {
-    if (whitelist.indexOf(origin) !== -1) {
-      callback(null, true);
-    } else {
-      callback(new Error("Not allowed by CORS"));
-    }
-  },
-};
+// var whitelist = [
+//   "https://audio--manager.herokuapp.com",
+//   "http://localhost:3000/audio/getAudios",
+// ];
+// var corsOptions = {
+//   origin: function (origin, callback) {
+//     if (whitelist.indexOf(origin) !== -1) {
+//       callback(null, true);
+//     } else {
+//       callback(new Error("Not allowed by CORS"));
+//     }
+//   },
+// };
 
-// app.all("/*", function (req, res, next) {
-//   res.header("Access-Control-Allow-Origin", "*");
+app.all("/*", function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
 
-//   res.header("Access-Control-Allow-Headers", "*");
-//   next();
-// });
-app.use(cors(corsOptions));
+  res.header("Access-Control-Allow-Headers", "*");
+  next();
+});
+app.use(cors());
 
 // app.use((req, res, next) => {
 //   const allowedOrigins = [
