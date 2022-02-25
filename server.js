cd5c79e34bf231e2136f17abaaeeeabc4e2e4362
Rutu Thakkar <rutu.thakkar@iviewlabs.net>
2022-02-25 11:22:26 +0530

    cors origin change

--- server.js
+++ server.js
@@ -54,15 +54,28 @@ const bodyParser = require("body-parser");
 //   })
 // );
 
+// app.use((req, res, next) => {
+//   //allow access from every, elminate CORS
+//   res.setHeader("Access-Control-Allow-Origin", "*");
+//   res.removeHeader("x-powered-by");
+//   //set the allowed HTTP methods to be requested
+//   res.setHeader("Access-Control-Allow-Methods", "POST");
+//   //headers clients can use in their requests
+//   res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+//   //allow request to continue and be handled by routes
+//   next();
+// });
+
 app.use((req, res, next) => {
-  //allow access from every, elminate CORS
   res.setHeader("Access-Control-Allow-Origin", "*");
-  res.removeHeader("x-powered-by");
-  //set the allowed HTTP methods to be requested
-  res.setHeader("Access-Control-Allow-Methods", "POST");
-  //headers clients can use in their requests
-  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
-  //allow request to continue and be handled by routes
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "OPTIONS, GET, POST, PUT, PATCH, DELETE"
+  );
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  if (req.method === "OPTIONS") {
+    return res.sendStatus(200);
+  }
   next();
 });
 
