251ab292ec3a9e92a9df7e4fb25b119a99189302
Rutu Thakkar <rutu.thakkar@iviewlabs.net>
2022-03-02 11:22:34 +0530

    cors option change

--- server.js
+++ server.js
@@ -56,17 +56,17 @@ app.options("*", cors());
 //   })
 // );
 
-// app.use((req, res, next) => {
-//   //allow access from every, elminate CORS
-//   res.setHeader("Access-Control-Allow-Origin", "*");
-//   res.removeHeader("x-powered-by");
-//   //set the allowed HTTP methods to be requested
-//   res.setHeader("Access-Control-Allow-Methods", "POST");
-//   //headers clients can use in their requests
-//   res.setHeader("Access-Control-Allow-Headers", "Content-Type");
-//   //allow request to continue and be handled by routes
-//   next();
-// });
+app.use((req, res, next) => {
+  //allow access from every, elminate CORS
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.removeHeader("x-powered-by");
+  //set the allowed HTTP methods to be requested
+  res.setHeader("Access-Control-Allow-Methods", "POST");
+  //headers clients can use in their requests
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+  //allow request to continue and be handled by routes
+  next();
+});
 
 // app.use((req, res, next) => {
 //   res.setHeader("Access-Control-Allow-Origin", "*");
